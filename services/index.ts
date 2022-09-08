3f2e4a1eb3854f0f35dd00e37f70c505b0d231df
Joe Klakus <joeyk2k@hotmail.com>
2022-09-08 09:50:03 +0100

    order

--- services/index.ts
+++ services/index.ts
@@ -5,7 +5,7 @@ const graphqlAPI: any = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 export const getPosts = async () => {
 	const query = gql`
 		query MyQuery {
-			postsConnection {
+			postsConnection(orderBy: publishedAt_DESC) {
 				edges {
 					node {
 						author {
