2fd4a897041c50147a282a950511cd64e908d84b
Joe Klakus <joeyk2k@hotmail.com>
2022-09-13 08:41:14 +0100

    adsense

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6386510566931206"
+          crossOrigin="anonymous"></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
