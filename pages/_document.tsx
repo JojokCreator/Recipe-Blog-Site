79e4abf315d96f3ff25a4c36513307520104c08c
Joe Klakus <joeyk2k@hotmail.com>
2022-11-11 09:42:20 +0100

    google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,15 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6386510566931206"
-          crossOrigin="anonymous"></script>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FLD2RHGS5P"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-FLD2RHGS5P');
+        </script>
       </Head>
       <body>
         <Main />
