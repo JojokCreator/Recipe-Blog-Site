3e53418f023d37a87de533f5cd7962974ab991b2
Joe Klakus <joeyk2k@hotmail.com>
2023-01-01 18:10:47 +0200

    added receipe structured data

--- components/PostDetail.tsx
+++ components/PostDetail.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import moment from 'moment';
-import Image from 'next/image';
+import moment from 'moment'
+import Image from 'next/image'
 
 interface Props {
   post: {
@@ -11,44 +11,57 @@ interface Props {
     createdAt: string
     author: { name: string; photo: { url: string } }
     categories: { name: string; slug: string }[]
-    content: {raw: {children: {type: string, children: any}[]} }
+    content: { raw: { children: { type: string; children: any }[] } }
   }
 }
 
 const PostDetail = ({ post }: Props) => {
-  const getContentFragment = (index: any, text:any, obj:any, type:any) => {
-    let modifiedText = text;
+  const getContentFragment = (index: any, text: any, obj: any, type: any) => {
+    let modifiedText = text
 
     if (obj) {
       if (obj.bold) {
-        modifiedText = (<b key={index}>{text}</b>);
+        modifiedText = <b key={index}>{text}</b>
       }
 
       if (obj.italic) {
-        modifiedText = (<em key={index}>{text}</em>);
+        modifiedText = <em key={index}>{text}</em>
       }
 
       if (obj.underline) {
-        modifiedText = (<u key={index}>{text}</u>);
+        modifiedText = <u key={index}>{text}</u>
       }
     }
 
     switch (type) {
       case 'heading-three':
-        return <h3 key={index} className="text-xl font-semibold mb-4">{modifiedText.map((item: any, i: number) => <React.Fragment key={i}>{item}</React.Fragment>)}</h3>;
+        return (
+          <h3 key={index} className="text-xl font-semibold mb-4">
+            {modifiedText.map((item: any, i: number) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </h3>
+        )
       case 'paragraph':
-        return <p key={index} className="mb-8">{modifiedText.map((item: any, i: number) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
+        return (
+          <p key={index} className="mb-8">
+            {modifiedText.map((item: any, i: number) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </p>
+        )
       case 'heading-four':
-        return <h4 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item: any, i: number) => <React.Fragment key={i}>{item}</React.Fragment>)}</h4>;
+        return (
+          <h4 key={index} className="text-md font-semibold mb-4">
+            {modifiedText.map((item: any, i: number) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </h4>
+        )
       case 'iframe':
         return (
-          <iframe 
-                src={obj.url}
-                width={obj.width}
-                height={obj.height}
-                >
-            </iframe>
-        );
+          <iframe key={index} src={obj.url} width={obj.width} height={obj.height}></iframe>
+        )
       case 'image':
         return (
           <Image
@@ -58,22 +71,23 @@ const PostDetail = ({ post }: Props) => {
             width={obj.width}
             src={obj.src}
           />
-        );
+        )
       default:
-        return modifiedText;
+        return modifiedText
     }
-  };
+  }
   return (
     <>
       <div className="bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8">
         <div className="relative flex justify-center overflow-hidden shadow-md">
-          <Image 
-          src={post.featuredImage.url} 
-          height="400"
-          width="640"
-          priority={true}
-          alt="" 
-          className="shadow-lg rounded-t-lg lg:rounded-lg"/>
+          <Image
+            src={post.featuredImage.url}
+            height="400"
+            width="640"
+            priority={true}
+            alt=""
+            className="shadow-lg rounded-t-lg lg:rounded-lg"
+          />
         </div>
         <div className="px-4 lg:px-0">
           <div className="flex items-center mb-8 w-full">
@@ -85,27 +99,43 @@ const PostDetail = ({ post }: Props) => {
                 className="align-middle rounded-full"
                 src={post.author.photo.url}
               />
-              <p className="inline align-middle text-gray-700 ml-2 font-medium text-lg">{post.author.name}</p>
+              <p className="inline align-middle text-gray-700 ml-2 font-medium text-lg">
+                {post.author.name}
+              </p>
             </div>
             <div className="font-medium text-gray-700">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-indigo-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 inline mr-2 text-indigo-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+                />
               </svg>
-              <span className="align-middle">{moment(post.createdAt).format('MMM DD, YYYY')}</span>
+              <span className="align-middle">
+                {moment(post.createdAt).format('MMM DD, YYYY')}
+              </span>
             </div>
           </div>
           <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
           {post.content.raw.children.map((typeObj, index) => {
-            const children = typeObj.children.map((item: any, itemindex: number) => 
-            getContentFragment(itemindex, item.text, item, null));
+            const children = typeObj.children.map(
+              (item: any, itemindex: number) =>
+                getContentFragment(itemindex, item.text, item, null)
+            )
 
-            return getContentFragment(index, children, typeObj, typeObj.type);
-          })} 
+            return getContentFragment(index, children, typeObj, typeObj.type)
+          })}
         </div>
       </div>
-
     </>
-  );
-};
+  )
+}
 
-export default PostDetail;
+export default PostDetail
--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useRouter } from "next/router";
+import { useRouter } from 'next/router'
 import {
   PostDetail,
   Categories,
@@ -8,58 +7,120 @@ import {
   Comments,
   CommentForm,
   Loader,
-} from "../../components";
-import { getPosts, getPostsDetails } from "../../services";
-import AdjacentPosts from "../../sections/AdjacentPosts";
-import Head from "next/head";
+} from '../../components'
+import { getPosts, getPostsDetails } from '../../services'
+import AdjacentPosts from '../../sections/AdjacentPosts'
+import Head from 'next/head'
 
 type SlugProps = {
   post: {
-    title: string;
-    exerpt: string;
-    featuredImage: { url: string };
-    slug: string;
-    createdAt: string;
-    author: { name: string; photo: { url: string }; bio: string };
-    categories: { name: string; slug: string }[];
-    content: { raw: { children: { type: string; children: any }[] } };
-  };
-};
+    title: string
+    exerpt: string
+    featuredImage: { url: string }
+    slug: string
+    createdAt: string
+    author: { name: string; photo: { url: string }; bio: string }
+    categories: { name: string; slug: string }[]
+    content: { raw: { children: { type: string; children: any }[] } }
+  }
+}
 
 type Params = {
   params: {
-    slug: string;
-  };
-};
+    slug: string
+  }
+}
 
 const PostDetails = ({ post }: SlugProps) => {
-  const router = useRouter();
+  const router = useRouter()
   if (router.isFallback) {
-    return <Loader />;
+    return <Loader />
   }
 
-  const structuredData = {
-    '@context': 'https://schema.org',
-    '@type': 'BlogPosting',
-    headline: post.title,
-    description: post.exerpt,
-    author: [
-      {
-        '@type': 'Person',
-        name: post.author.name,
-      },
-    ],
-    image: post.featuredImage.url,
-    datePublished: post.createdAt,
-  };
+  const steps = [];
+  const ingredients = [];
+  const array = post.content.raw.children
 
+  const startIndex = array.findIndex(obj => obj.children[0].text.toLowerCase() === 'ingredients');
+  const endIndex = array.findIndex(obj => obj.children[0].text.toLowerCase() === 'instructions');
+  const ingredArray = array.slice(startIndex, endIndex)
+  for (let i = 0; i<ingredArray.length; i++){
+    if(ingredArray[i].children[0].text != ""){
+      ingredients.push(ingredArray[i].children[0].text)
+    }
+  }
+  
+  const index = array.findIndex(obj => obj.type === 'image');
+  const newArray = array.slice(index)
+  for (let i = 0; i<newArray.length; i++){
+    if(newArray[i].children[0].text != ""){
+      steps.push({"@type": "HowToStep", "text": newArray[i].children[0].text})
+    }
+  }
+ 
+
+  const structuredData = {
+    "@context": "https://schema.org/",
+    "@type": "Recipe",
+    "name": post.title,
+    "image": [ post.featuredImage.url ],
+    "author": {
+      "@type": "Person",
+      "name": post.author.name
+    },
+    "datePublished": post.createdAt,
+    "description": post.exerpt,
+    "recipeCuisine": post.categories[0].name,
+    "prepTime": "PT1M",
+    "cookTime": "PT2M",
+    "totalTime": "PT3M",
+    //"keywords": "non-alcoholic",
+    "recipeYield": "1 serving",
+    "recipeCategory": post.categories[0].name,
+    // "nutrition": {
+    //   "@type": "NutritionInformation",
+    //   "calories": "120 calories"
+    // },
+    // "aggregateRating": {
+    //   "@type": "AggregateRating",
+    //   "ratingValue": "5",
+    //   "ratingCount": "18"
+    // },
+    "recipeIngredient": ingredients,
+    "recipeInstructions": steps,
+    // "video": {
+    //   "@type": "VideoObject",
+    //   "name": "How to make a Party Coffee Cake",
+    //   "description": "This is how you make a Party Coffee Cake.",
+    //   "thumbnailUrl": [
+    //     "https://example.com/photos/1x1/photo.jpg",
+    //     "https://example.com/photos/4x3/photo.jpg",
+    //     "https://example.com/photos/16x9/photo.jpg"
+    //    ],
+    //   "contentUrl": "https://www.example.com/video123.mp4",
+    //   "embedUrl": "https://www.example.com/videoplayer?video=123",
+    //   "uploadDate": "2018-02-05T08:00:00+08:00",
+    //   "duration": "PT1M33S",
+    //   "interactionStatistic": {
+    //     "@type": "InteractionCounter",
+    //     "interactionType": { "@type": "WatchAction" },
+    //     "userInteractionCount": 2347
+    //   },
+    //   "expires": "2019-02-05T08:00:00+08:00"
+    //  }
+  }
+
+  console.log(structuredData)
   return (
     <>
       <div className="container mx-auto px-10 mb-8">
         <Head>
           <meta charSet="utf-8" />
           <title>{post.title}</title>
-          <link rel="canonical" href={"https://www.barefootrecipe.com/post/"+ post.slug}   />
+          <link
+            rel="canonical"
+            href={'https://www.barefootrecipe.com/post/' + post.slug}
+          />
           <meta name="description" content={post.exerpt} />
 
           <meta name="theme-color" content="#000000" />
@@ -73,18 +134,14 @@ const PostDetails = ({ post }: SlugProps) => {
           {/* twitter tags */}
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:title" content={post.title} />
-          <meta
-            name="twitter:description"
-            content={post.exerpt}
-          />
+          <meta name="twitter:description" content={post.exerpt} />
           <meta name="twitter:image" content={post.featuredImage.url} />
 
           <script
-          key="structured-data"
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
+            key="structured-data"
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
           />
-
         </Head>
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
           <div className="col-span-1 lg:col-span-8">
@@ -106,25 +163,25 @@ const PostDetails = ({ post }: SlugProps) => {
         </div>
       </div>
     </>
-  );
-};
-export default PostDetails;
+  )
+}
+export default PostDetails
 
 export const getStaticProps = async ({ params }: Params) => {
-  const data = await getPostsDetails(params.slug);
+  const data = await getPostsDetails(params.slug)
   return {
     props: {
       post: data,
     },
-  };
-};
+  }
+}
 
 // Specify dynamic routes to pre-render pages based on data.
 // The HTML is generated at build time and will be reused on each request.
 export async function getStaticPaths() {
-  const posts: [] = await getPosts();
+  const posts: [] = await getPosts()
   return {
     paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
     fallback: true,
-  };
+  }
 }
