386c766b53e6e981f86bacb7d4237b33ed822c06
Joe Klakus <joeyk2k@hotmail.com>
2023-01-15 21:08:03 +0200

    ignore act error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  images: {
-    domains: ["media.graphassets.com"],
-  },
-  reactStrictMode: true,
+// /** @type {import('next').NextConfig} */
+// module.exports = {
+//   images: {
+//     domains: ["media.graphassets.com"],
+//   },
+//   reactStrictMode: true,
+// }
+
+module.exports = async (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    images: {
+      domains: ["media.graphassets.com"],
+    },
+    reactStrictMode: true,
+  }
+  return nextConfig
 }
