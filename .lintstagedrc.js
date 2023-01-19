8bf4977d8791a8567822a579701367fb7c7e7871
jojokCreator <joeyk2k@gmail.com>
2023-01-19 17:11:45 +0200

    api routes

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -4,11 +4,11 @@ module.exports = {
 
   // Lint & Prettify TS and JS files
   '**/*.(ts|tsx|js)': (filenames) => [
-    `yarn eslint ${filenames.join(' ')}`,
-    `yarn prettier --write ${filenames.join(' ')}`,
+    `npm run eslint ${filenames.join(' ')}`,
+    `npm run prettier --write ${filenames.join(' ')}`,
   ],
 
   // Prettify only Markdown and JSON files
   '**/*.(md|json)': (filenames) =>
-    `yarn prettier --write ${filenames.join(' ')}`,
+    `npm run prettier --write ${filenames.join(' ')}`,
 }
