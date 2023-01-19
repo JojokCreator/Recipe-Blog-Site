b3c813357d044916a02aa61852ccd7a285a0130e
jojokCreator <joeyk2k@gmail.com>
2023-01-19 17:14:32 +0200

    husky

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,14 +1,5 @@
 module.exports = {
-  // Type check TypeScript files
-  '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
-
-  // Lint & Prettify TS and JS files
-  '**/*.(ts|tsx|js)': (filenames) => [
-    `npm run eslint ${filenames.join(' ')}`,
-    `npm run prettier --write ${filenames.join(' ')}`,
-  ],
-
-  // Prettify only Markdown and JSON files
-  '**/*.(md|json)': (filenames) =>
-    `npm run prettier --write ${filenames.join(' ')}`,
+  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'eslint'],
+  '**/*.ts?(x)': () => 'npm run check-types',
+  '*.{json,yaml}': ['prettier --write'],
 }
--- pages/api/reddit.tsx
+++ pages/api/reddit.tsx
@@ -7,22 +7,25 @@ const r = new snoowrap({
   clientId: process.env.REDDIT_APP_ID,
   clientSecret: process.env.REDDIT_SECRET,
   username: process.env.REDDIT_USER,
-  password: process.env.REDDIT_PASSWORD
-});
+  password: process.env.REDDIT_PASSWORD,
+})
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  
   const videos = await youtube()
-  const postNumber = await r.getUser(process.env.REDDIT_USER as string).getSubmissions()
-  const post = (postNumber.length-14);
+  const postNumber = await r
+    .getUser(process.env.REDDIT_USER as string)
+    .getSubmissions()
+  const post = postNumber.length - 14
 
-  r.getSubreddit("cookingvideos").submitLink({
-    subredditName: "cookingvideos",
+  r.getSubreddit('cookingvideos').submitLink({
+    subredditName: 'cookingvideos',
     title: videos.items[post].snippet.title,
     url: `https://www.youtube.com/watch?v=${videos.items[post].snippet.resourceId.videoId}`,
     sendReplies: true,
   })
 
-  res.status(200).json(videos.items[post].snippet.title + " posted successfully")
+  res
+    .status(200)
+    .json(videos.items[post].snippet.title + ' posted successfully')
 }
 export default handler
