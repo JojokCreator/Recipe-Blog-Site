70a10e3a73a1596fa63d8d69a701f6403d366650
Joe Klakus <joeyk2k@hotmail.com>
2023-01-22 15:43:29 +0200

    fixed reddit endpoint

--- pages/api/reddit.tsx
+++ pages/api/reddit.tsx
@@ -17,12 +17,12 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const postNumber = parseInt((await get('reddit_number')) as string)
 
   //FoodVideos
-  // r.getSubreddit('cookingvideos').submitLink({
-  //   subredditName: 'cookingvideos',
-  //   title: videos.items[postNumber].snippet.title,
-  //   url: `https://www.youtube.com/watch?v=${videos.items[postNumber].snippet.resourceId.videoId}`,
-  //   sendReplies: true,
-  // })
+  r.getSubreddit('cookingvideos').submitLink({
+    subredditName: 'cookingvideos',
+    title: videos.items[postNumber].snippet.title,
+    url: `https://www.youtube.com/watch?v=${videos.items[postNumber].snippet.resourceId.videoId}`,
+    sendReplies: true,
+  })
 
   await incrementPost(postNumber, 'reddit_number')
 
